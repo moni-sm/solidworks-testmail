a4e55a3559d79033e2d08bf03a9f29b2668eaac5
moni-sm <152909890+moni-sm@users.noreply.github.com>
2025-10-03 10:34:28 +0530

    Update AdminPage.jsx

--- solidworks-event/admin/src/components/AdminPage.jsx
+++ solidworks-event/admin/src/components/AdminPage.jsx
@@ -5,7 +5,7 @@ import "./AdminPage.css";
 export default function AdminPage() {
   const [registrations, setRegistrations] = useState([]);
   const [selectedIds, setSelectedIds] = useState([]);
-  const API_URL = "https://solidworks-testmail.onrender.com";
+  const API_URL = "http://localhost:5000";
 
   const fetchRegistrations = async () => {
     try {
@@ -88,4 +88,5 @@ export default function AdminPage() {
     </div>
   );
 }
+
 
